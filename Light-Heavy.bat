fd180809b68210b647642cfa38b9078460ee986e
Raz <razash10@gmail.com>
2021-01-01 20:12:40 +0200

    Implemented HeavyPlayer and LightPlayer

---
+++ Light-Heavy.bat
@@ -0,0 +1,2 @@
+python main.py -board default_board.csv -player1 LightABPlayer -player2 HeavyABPlayer
+pause
--- players/LightABPlayer.py
+++ players/LightABPlayer.py
@@ -2,14 +2,27 @@
 MiniMax Player with AlphaBeta pruning with light heuristic
 """
 from players.AbstractPlayer import AbstractPlayer
-#TODO: you can import more modules, if needed
+# TODO: you can import more modules, if needed
+from SearchAlgos import AlphaBeta
+import numpy as np
+import time
+import random
+import utils
 
 
 class Player(AbstractPlayer):
     def __init__(self, game_time, penalty_score):
-        AbstractPlayer.__init__(self, game_time, penalty_score) # keep the inheritance of the parent's (AbstractPlayer) __init__()
-        #TODO: initialize more fields, if needed, and the AlphaBeta algorithm from SearchAlgos.py
-
+        AbstractPlayer.__init__(self, game_time,
+                                penalty_score)  # keep the inheritance of the parent's (AbstractPlayer) __init__()
+        # TODO: initialize more fields, if needed, and the AlphaBeta algorithm from SearchAlgos.py
+        self.penalty_score = penalty_score
+        self.board = None
+        self.pos = None
+        self.rival_pos = None
+        self.fruits_dict = {}
+        self.fruits_ate = {}
+        self.rival_fruits_ate = {}
+        self.first_turn = True
 
     def set_game_params(self, board):
         """Set the game parameters needed for this player.
@@ -19,8 +32,13 @@ class Player(AbstractPlayer):
             - board: np.array, a 2D matrix of the board.
         No output is expected.
         """
-        #TODO: erase the following line and implement this function.
-        raise NotImplementedError
+        # TODO: erase the following line and implement this function.
+        self.board = board
+        pos = np.where(board == 1)
+        rival_pos = np.where(board == 2)
+        # convert pos to tuple of ints
+        self.pos = tuple(ax[0] for ax in pos)
+        self.rival_pos = tuple(ax[0] for ax in rival_pos)
 
     def make_move(self, time_limit, players_score):
         """Make move with this Player.
@@ -29,9 +47,40 @@ class Player(AbstractPlayer):
         output:
             - direction: tuple, specifing the Player's movement, chosen from self.directions
         """
-        #TODO: erase the following line and implement this function.
-        raise NotImplementedError
+        # TODO: erase the following line and implement this function.
+        start_time = time.time()
+        depth = 1
+        best_direction = None
+        best_score = float('-inf')
+        limit = 4
 
+        print()
+        print("LightPlayer turn:")
+
+        # At least "buffer" in ms left to run
+        while depth <= limit:
+            minimax_algo = AlphaBeta(self.utility, self.succ, self.perform_move, None)
+            state = [self.pos, self.rival_pos, start_time, time_limit]
+            score, direction = minimax_algo.search(state, depth, True)
+            print('score=' + str(score) + ' direction=' + str(direction) + ' depth=' + str(depth))
+            depth += 1
+            if best_direction is None or score > best_score:
+                best_score = score
+                best_direction = direction
+
+        if best_direction is None:
+            available_moves = self.succ(self.pos)
+            random.shuffle(available_moves)
+            best_direction = available_moves[0]
+
+        i = self.pos[0] + best_direction[0]
+        j = self.pos[1] + best_direction[1]
+
+        self.perform_move(self.pos, (i, j))
+
+        self.pos = (i, j)
+
+        return best_direction
 
     def set_rival_move(self, pos):
         """Update your info, given the new position of the rival.
@@ -39,9 +88,12 @@ class Player(AbstractPlayer):
             - pos: tuple, the new position of the rival.
         No output is expected
         """
-        #TODO: erase the following line and implement this function.
-        raise NotImplementedError
-
+        # TODO: erase the following line and implement this function.
+        if self.first_turn:
+            self.first_turn = False
+            if self.board[pos] > 0:
+                self.rival_fruits_ate[pos] = self.board[pos]
+        self.perform_move(self.rival_pos, pos)
 
     def update_fruits(self, fruits_on_board_dict):
         """Update your info on the current fruits on board (if needed).
@@ -51,14 +103,88 @@ class Player(AbstractPlayer):
                                     'value' is the value of this fruit.
         No output is expected.
         """
-        #TODO: erase the following line and implement this function. In case you choose not to use this function, 
+        # TODO: erase the following line and implement this function. In case you choose not to use this function,
         # use 'pass' instead of the following line.
-        raise NotImplementedError
-
+        self.fruits_dict = fruits_on_board_dict
 
     ########## helper functions in class ##########
-    #TODO: add here helper functions in class, if needed
+    # TODO: add here helper functions in class, if needed
+    @staticmethod
+    def count_ones(board):
+        counter = len(np.where(board == 1)[0])
+        return counter
 
+    def h_fruits_are_yummy(self, pos):
+        if pos in self.fruits_dict:
+            return self.fruits_dict[pos]
+        elif pos in self.fruits_ate:
+            return self.fruits_ate[pos]
+        elif pos in self.rival_fruits_ate:
+            return self.rival_fruits_ate[pos]
+        else:
+            return 0
+
+    def h_simple_player(self, pos):
+        num_steps_available = len(self.succ(pos))
+
+        if num_steps_available == 0:
+            return -1
+        else:
+            return 4 - num_steps_available
 
     ########## helper functions for AlphaBeta algorithm ##########
-    #TODO: add here the utility, succ, and perform_move functions used in AlphaBeta algorithm
+    # TODO: add here the utility, succ, and perform_move functions used in AlphaBeta algorithm
+    def utility(self, state):
+        pos = state[0]
+        v1 = self.h_fruits_are_yummy(pos)
+        v2 = self.h_simple_player(pos)
+        return v1 + v2
+
+    def succ(self, pos):
+        next_poses = []
+
+        for d in self.directions:
+            i = pos[0] + d[0]
+            j = pos[1] + d[1]
+
+            # check legal move
+            if 0 <= i < len(self.board) and 0 <= j < len(self.board[0]) and (self.board[i][j] not in [-1, 1, 2]):
+                next_pos = (i, j)
+                next_poses.append(next_pos)
+
+        return next_poses
+
+    def perform_move(self, pos, next_pos):
+        assert (self.board[pos] in [1, 2])
+        assert (self.board[next_pos] not in [1, 2])
+
+        player_index = self.board[pos]
+
+        if self.board[next_pos] != -1:  # moving forward
+            self.board[pos] = -1
+            if next_pos in self.fruits_dict:
+                fruit_val = self.fruits_dict.pop(next_pos)
+                if player_index == 1:
+                    self.fruits_ate[next_pos] = fruit_val
+                elif player_index == 2:
+                    self.rival_fruits_ate[next_pos] = fruit_val
+
+        else:  # returning backward
+            if pos in self.fruits_ate:
+                fruit_val = self.fruits_ate.pop(pos)
+                self.board[pos] = fruit_val
+                self.fruits_dict[pos] = fruit_val
+            elif pos in self.rival_fruits_ate:
+                fruit_val = self.rival_fruits_ate.pop(pos)
+                self.board[pos] = fruit_val
+                self.fruits_dict[pos] = fruit_val
+            else:
+                self.board[pos] = 0
+
+        self.board[next_pos] = player_index
+
+        if player_index == 1:
+            self.pos = next_pos
+        elif player_index == 2:
+            self.rival_pos = next_pos
+
